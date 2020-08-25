3d323eb378fee2684544312ef7146bf4448acf41
chris9803 <t109598072@ntut.org.com>
2020-08-25 10:06:00 +0800

    modify unit test

---
+++ src/test/java/OPinTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class IPinTest
+{
+    @Test
+    public void testSetInputValue()
+    {
+        //Arrange
+        IPin uat = new IPin();
+
+        //Act
+        uat.setInputValue(1);
+
+        //Assert
+        assertEquals(uat.getOutput(), 1);
+    }
+}
